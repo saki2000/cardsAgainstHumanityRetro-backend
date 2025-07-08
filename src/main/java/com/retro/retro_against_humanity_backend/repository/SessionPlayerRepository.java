7c54db22710dc530fd58ec5f71801a2d8bf2b9d7
Michal Sakowski <sakijudo@gmail.com>
2025-07-08 10:56:24 +0100

    added db interfaces

---
+++ src/main/java/com/retro/retro_against_humanity_backend/repository/SessionPlayerRepository.java
@@ -0,0 +1,16 @@
+package com.retro.retro_against_humanity_backend.repository;
+
+import com.retro.retro_against_humanity_backend.entity.ActiveSession;
+import com.retro.retro_against_humanity_backend.entity.SessionPlayer;
+import com.retro.retro_against_humanity_backend.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SessionPlayerRepository extends JpaRepository<SessionPlayer, Long> {
+    boolean existsByUserAndSession(Users user, ActiveSession session);
+    List<SessionPlayer> findBySession(ActiveSession session);
+    void deleteByUserAndSession(Users user, ActiveSession session);
+}
