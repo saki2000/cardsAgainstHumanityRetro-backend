f2db24d9b77116b9dd09eafbb81b5a0bfa24b256
Michal Sakowski <sakijudo@gmail.com>
2025-07-09 22:13:58 +0100

    added sql migrations

--- src/main/resources/db/migration/V1_2__AlterTableWithNewColumns.sql
+++ src/main/resources/db/migration/V1_2__AlterTableWithNewColumns.sql
@@ -1,4 +1,6 @@
 ALTER TABLE active_session ADD COLUMN host_user_id INTEGER REFERENCES users(id) NULL;
+ALTER TABLE active_session ADD COLUMN current_player_id INTEGER REFERENCES users(id) NULL;
 ALTER TABLE session_players ADD COLUMN created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE session_players ADD COLUMN turn_order INTEGER NOT NULL;
 ALTER TABLE active_session DROP COLUMN email;
 ALTER TABLE active_session DROP COLUMN username;
