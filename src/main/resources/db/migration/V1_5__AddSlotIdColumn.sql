e1490b5de60e01946322c32f26e18864d198976b
Michal Sakowski <sakijudo@gmail.com>
2025-07-14 15:39:05 +0100

    added migrations

---
+++ src/main/resources/db/migration/V1_5__AddSlotIdColumn.sql
@@ -0,0 +1,1 @@
+ALTER TABLE session_card ADD COLUMN slot_id TEXT;
--- src/main/resources/db/migration/V1_5__AdditionalColumnsInSessionCards.sql
+++
@@ -1,4 +0,0 @@
-CREATE TYPE card_status AS ENUM ('FREE', 'PLAYED');
-
-ALTER TABLE session_card
-    ADD COLUMN status card_status NOT NULL DEFAULT 'FREE';
